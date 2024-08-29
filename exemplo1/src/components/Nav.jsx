cdf865facf11dfce9222cd5cd2ab6d59bcd9609f
Fernando Navajas Moraes <fernandonm07@hotmail.com>
2024-08-29 09:55:29 -0300

    Nav estilizado

--- exemplo1/src/components/Nav.jsx
+++ exemplo1/src/components/Nav.jsx
@@ -1,9 +1,14 @@
 import {Link} from 'react-router-dom'
+import { NavStyle } from '../css/NavStyle'
 
 const Nav=()=>{
     return(
         <>
-            <Link to='/'>Home</Link>
+            <NavStyle>
+            <section className="nav">
+                <Link to='/'className='link'>Home</Link>
+            </section>
+            </NavStyle>
         </>
     )
 }
---
+++ exemplo1/src/css/NavStyle.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const NavStyle = styled.section `
+
+.nav{
+    width:100%;
+    height:70px;
+    list-style:none;
+    gap:1rem;
+    background-image:linear-gradient(to left, aqua, blue);  
+}
+
+.link{
+    font-size:1rem;
+    color:#fff;
+    font-weight:700;
+    padding:20px;
+    text-transfrom:uppercase;
+    text-decoration:none;
+}
+
+
+`
