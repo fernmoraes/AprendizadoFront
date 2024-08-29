97da7ea672d934e8b671335122da8daa4efa5de9
Fernando Navajas Moraes <fernandonm07@hotmail.com>
2024-08-29 09:21:44 -0300

    Main alterado

--- exemplo1/src/main.jsx
+++ exemplo1/src/main.jsx
@@ -1,9 +1,26 @@
-import { StrictMode } from 'react'
+import {StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
+import Error from './routes/Error.jsx'
+
+const router= createBrowserRouter([
+  {
+    //Chamando Elemento Pai
+    path: '/', element:<App/>,
+    errorElement:<Error/>,
+
+    //Chamando Elementos Filhos
+
+    children:[
+      {path: '/home',element:<Home/>}
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/*Renderizando o routerProvider*/}
+    <RouterProvider router={router} />
   </StrictMode>,
 )
