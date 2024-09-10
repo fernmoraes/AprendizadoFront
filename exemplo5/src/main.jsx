936704074e02f02aab6381ed2c37494358022283
Fernando Navajas Moraes <fernandonm07@hotmail.com>
2024-09-10 11:05:13 -0300

    Rotas criadas e exportadas

--- exemplo5/src/main.jsx
+++ exemplo5/src/main.jsx
@@ -1,9 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
+import Error from './routes/Error.jsx'
+import Login from './routes/Login.jsx'
+import Dashboard from './routes/Dashboard.jsx'
+import Home from './routes/Home.jsx'
+
+//Criando a função que vai criar as rotas
+
+const router = createBrowserRouter([
+  {
+    //Elementos Pai
+    path:'/', element:<App/>,
+    errorElement:<Error/>,
+
+    //Elementos Filho
+    children:[
+      {path:'/',element:<Home/>},
+      {path:'/login', element:<Login/>},
+      {path:'/dashboard',element:<Dashboard/>},
+    ]
+
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
