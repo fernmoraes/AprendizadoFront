e1bdee6e2c9ad5c9381d6714b32ff016a3f79bd7
Fernando Navajas Moraes <fernandonm07@hotmail.com>
2024-09-19 09:51:56 -0300

    home arrumada

--- exemplo6/src/routes/Home.jsx
+++ exemplo6/src/routes/Home.jsx
@@ -1,8 +1,9 @@
-import '../css/loja.css'
 import Imagem1 from '../assets/Imagem1.jpg'
-import Imagem2 from'../assets/Imagem2.jpg'
+import Imagem2 from '../assets/Imagem2.jpg'
 import Imagem3 from '../assets/Imagem3.jpg'
 import Imagem4 from '../assets/Imagem4.jpg'
+
+import '../css/loja.css'
 
 
 const Home = () => {
@@ -14,7 +15,7 @@ const Home = () => {
             <main>
                 <article className="produto-grid">
 
-                    <section className="produto-item" >
+                    <section className="produto-item span-row-2" >
                         <img src={Imagem1} alt="Produto-1" id="image" />
                         <div>
                             <h3>Produto-1</h3>
@@ -22,96 +23,88 @@ const Home = () => {
                         </div>
                     </section>
 
-                        <section className="produto-item ">
-                            <img src={Imagem2} alt="Produto-2" id="image" />
-                            <div>
-                                <h3>Produto-2</h3>
-                                <p>R$ 200,00</p>
-                            </div>
-                        </section>
+                    <section className="produto-item ">
+                        <img src={Imagem2} alt="Produto-2" id="image" />
+                        <div>
+                            <h3>Produto-2</h3>
+                            <p>R$ 200,00</p>
+                        </div>
+                    </section>
 
-                        <section className="produto-item ">
-                            <img src={Imagem3} alt="Produto-2" id="image" />
-                            <div>
-                                <h3>Produto-3</h3>
-                                <p>R$ 300,00</p>
-                            </div>
-                        </section>
+                    <section className="produto-item">
+                        <img src={Imagem3} alt="Produto-3" id="image" />
+                        <div>
+                            <h3>Produto-3</h3>
+                            <p>R$ 300,00</p>
+                        </div>
+                    </section>
 
-                        <section className="produto-item ">
-                            <img src={Imagem4} alt="Produto-2" id="image" />
-                            <div>
-                                <h3>Produto-4</h3>
-                                <p>R$ 400,00</p>
-                            </div>
-                        </section>
+                    <section className="produto-item span-row-2">
+                        <img src={Imagem4} alt="Produto-4" id="image" />
+                        <div>
+                            <h3>Produto-4</h3>
+                            <p>R$ 390,00</p>
+                        </div>
+                    </section>
 
-                        <section className="produto-item ">
-                            <img src={Imagem1} alt="Produto-2" id="image" />
-                            <div>
-                                <h3>Produto-5</h3>
-                                <p>R$ 500,00</p>
-                            </div>
-                        </section>
+                    <section className="produto-item span-row-2 span-col-2">
+                        <img src={Imagem3} alt="Produto-5" id="image" />
+                        <div>
+                            <h3>Produto-5</h3>
+                            <p>R$ 400,00</p>
+                        </div>
+                    </section>
 
-                        <section className="produto-item ">
-                            <img src={Imagem2} alt="Produto-2" id="image" />
-                            <div>
-                                <h3>Produto-6</h3>
-                                <p>R$ 600,00</p>
-                            </div>
-                        </section>
 
-                        <section className="produto-item ">
-                            <img src={Imagem3} alt="Produto-2" id="image" />
-                            <div>
-                                <h3>Produto-7</h3>
-                                <p>R$ 700,00</p>
-                            </div>
-                        </section>
+                    <section className="produto-item">
+                        <img src={Imagem2} alt="Produto-6" id="image" />
+                        <div>
+                            <h3>Produto-6</h3>
+                            <p>R$ 180,00</p>
+                        </div>
+                    </section>
 
-                        <section className="produto-item ">
-                            <img src={Imagem4} alt="Produto-2" id="image" />
-                            <div>
-                                <h3>Produto-8</h3>
-                                <p>R$ 800,00</p>
-                            </div>
-                        </section>
+                    <section className="produto-item">
+                        <img src={Imagem1} alt="Produto-7" id="image" />
+                        <div>
+                            <h3>Produto-7</h3>
+                            <p>R$ 650,00</p>
+                        </div>
+                    </section>
 
-                        <section className="produto-item ">
-                            <img src={Imagem1} alt="Produto-2" id="image" />
-                            <div>
-                                <h3>Produto-9</h3>
-                                <p>R$ 900,00</p>
-                            </div>
-                        </section>
+                    <section className="produto-item">
+                        <img src={Imagem4} alt="Produto-8" id="image" />
+                        <div>
+                            <h3>Produto-8</h3>
+                            <p>R$ 170,00</p>
+                        </div>
+                    </section>
 
-                        <section className="produto-item ">
-                            <img src={Imagem2} alt="Produto-2" id="image" />
-                            <div>
-                                <h3>Produto-10</h3>
-                                <p>R$ 1000,00</p>
-                            </div>
-                        </section>
+                    <section className="produto-item">
+                        <img src={Imagem3} alt="Produto-9" id="image" />
+                        <div>
+                            <h3>Produto-9</h3>
+                            <p>R$ 800,00</p>
+                        </div>
+                    </section>
 
-                        <section className="produto-item ">
-                            <img src={Imagem3} alt="Produto-2" id="image" />
-                            <div>
-                                <h3>Produto-11</h3>
-                                <p>R$ 1100,00</p>
-                            </div>
-                        </section>
+                    <section className="produto-item">
+                        <img src={Imagem2} alt="Produto-10" id="image" />
+                        <div>
+                            <h3>Produto-10</h3>
+                            <p>R$ 630,00</p>
+                        </div>
+                    </section>
 
-                        <section className="produto-item ">
-                            <img src={Imagem4} alt="Produto-2" id="image" />
-                            <div>
-                                <h3>Produto-12</h3>
-                                <p>R$ 1200,00</p>
-                            </div>
-                        </section>
-                        
+                    <section className="produto-item">
+                        <img src={Imagem1} alt="Produto-11" id="image" />
+                        <div>
+                            <h3>Produto-11</h3>
+                            <p>R$ 542,00</p>
+                        </div>
+                    </section>
+
                 </article>
-
             </main>
         </>
     )
