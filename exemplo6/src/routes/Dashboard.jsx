407499d8223495cc463ee81ce2a8ddac383d276f
Fernando Navajas Moraes <fernandonm07@hotmail.com>
2024-09-19 08:40:56 -0300

    Botão de logout funcionando

--- exemplo6/src/routes/Dashboard.jsx
+++ exemplo6/src/routes/Dashboard.jsx
@@ -1,7 +1,23 @@
+import { CiLogout } from "react-icons/ci";
+import { useNavigate } from "react-router-dom"
+
 const Dashboard =()=>{
+
+    //Hook-useNavigate redireciona o componente
+    const navigate = useNavigate();
+    //criando a função handleLogout
+    const handleLogout=()=>{
+        sessionStorage.removeItem("usuário");
+        sessionStorage.removeItem("senha");
+        alert("by by...");
+        navigate("/");
+}
     return(
         <>
         <h1>Dashboard</h1>
+        <button onClick={handleLogout}>
+            <CiLogout/>
+        </button>
         </>
     )
 }
