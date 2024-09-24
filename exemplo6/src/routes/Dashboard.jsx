530481aa89cfc3571c001fa933f67f8d6d855199
Fernando Navajas Moraes <fernandonm07@hotmail.com>
2024-09-24 10:31:54 -0300

    Projeto atualizado

--- exemplo6/src/routes/Dashboard.jsx
+++ exemplo6/src/routes/Dashboard.jsx
@@ -1,23 +1,30 @@
-import { CiLogout } from "react-icons/ci";
-import { useNavigate } from "react-router-dom"
+import { IoMdLogOut } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+
 
 const Dashboard =()=>{
 
-    //Hook-useNavigate redireciona o componente
-    const navigate = useNavigate();
-    //criando a função handleLogout
-    const handleLogout=()=>{
-        sessionStorage.removeItem("usuário");
-        sessionStorage.removeItem("senha");
-        alert("by by...");
-        navigate("/");
+//Hook-useNavigate redireciona o componente
+const navigate = useNavigate();
+
+//criando a função handleLogout
+const handleLogout=()=>{
+
+    sessionStorage.removeItem("usuario");
+    sessionStorage.removeItem("senha");
+    alert("by by...");
+    navigate("/");
+
+    
 }
+
     return(
         <>
         <h1>Dashboard</h1>
         <button onClick={handleLogout}>
-            <CiLogout/>
+            <IoMdLogOut />
         </button>
+
         </>
     )
 }
